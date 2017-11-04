b4b3246bd25630f433896ebdddc3cee3508f6d4a
yarno <minejarno@gmail.com>
2017-11-04 18:45:45 +0100

    mobile fix

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,6 +1,11 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Yarno Bachmann">
+<meta name="description" content="Yarno Bachmann webdeveloper. Wilt U een mooie responsive website? Laat gerust een berricht achter.">
+<meta name="keywords" content="webdesign, yarnobachmann, bachmann, yarno, responsive, programmeur, projects, emmen, drenthe, code, web, king, design, mobile, kings, website, laravel">
+<meta name="Resource-type" content="Document">
+
 
 <title>Yarno's Portfolio @yield('title')</title>
 
